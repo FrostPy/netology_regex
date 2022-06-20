7621e5ca439c7b014449fce9121bec0dc482ed0c
Frost <ua_frost@mail.ru>
2022-06-20 09:19:38 +0300

    two commit

--- regex_dz.py
+++ regex_dz.py
@@ -1,60 +1,41 @@
 import re
+from pprint import pprint
 import csv
 
-DATA_FILE = 'phonebook_raw.csv'
-PHONE_PATTERN = '(8|\+7)?\s*(\(*)(\d{3})(\)*)(\s*|-)(\d{3})(\s*|-)(\d{2})(\s*|-)(\d{2})\s*(\(*)(\w\w\w\.)*\s*(\d{4})*(\))*'
-SUB_PHONE = r'+7(\3)\6-\8-\10 \12\13'
+with open("phonebook_raw.csv", encoding='utf8') as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
+# TODO 1: выполните пункты 1-3 ДЗ
+phone_pattern = re.compile(
+    r'(\+7|8)\s*\(?(\d{3})\)?(\s*|-)(\d{3})(\s*|-*)(\d{2})-?(\d{2})(\s*(\(?(\доб.)?)\s*(\d{4}))?(\))*')
+text_pattern = re.compile(
+    r'(\w+[А-яЁё])\s*\,*(\w+[А-яЁё])\s*\,*(\w+[А-яЁё])*\,*(\w+[А-яЁё])*\,*(\w+[А-яЁё]\w+[А-яЁё –]*\–*\s*)*\,*(\+*\d\s*\(*\d+\)*\-*\s*\d+\-*\d+\-*\d+\s*\(*\w*\.*\s*\d*\)*)*\,*(\w+\.*\w*\@\w+\.\w+)*')
 
-def input_data():
-    with open(DATA_FILE, encoding='utf-8') as f:
-        rows = csv.reader(f, delimiter=",")
-        contacts_list = list(rows)
-    return contacts_list
+new_contacts_list = []
+for i in range(len(contacts_list)):
+    if i == 0:
+        new_contacts_list.append(contacts_list[i])
+    else:
+        line = ','.join(contacts_list[i])
+        result = re.search(text_pattern, line)
+        new_contacts_list.append(list(result.groups()))
+        if new_contacts_list[i][0] in new_contacts_list:
+            print(new_contacts_list[i][0:3])
+        if new_contacts_list[i][5] != None:
+            new_contacts_list[i][5] = phone_pattern.sub(r'+7(\2)\4-\6-\7 \10\11', new_contacts_list[i][5])
 
+final_contacts_list = []
+for i in range(len(new_contacts_list)):
+    for j in range(len(new_contacts_list)):
+        if new_contacts_list[i][0] == new_contacts_list[j][0]:
+            new_contacts_list[i] = [x or y for x, y in zip(new_contacts_list[i], new_contacts_list[j])]
+    if new_contacts_list[i] not in final_contacts_list:
+        final_contacts_list.append(new_contacts_list[i])
 
-def parse_contact_list(contacts_list):
-    new_contacts_list = list()
-    for contact in contacts_list:
-        new_contact = list()
-        full_name_str = ",".join(contact[:3])
-        result = re.findall(r'(\w+)', full_name_str)
-        while len(result) < 3:
-            result.append('')
-        new_contact += result
-        new_contact.append(contact[3])
-        new_contact.append(contact[4])
-        phone_pattern = re.compile(PHONE_PATTERN)
-        changed_phone = phone_pattern.sub(SUB_PHONE, contact[5])
-        new_contact.append(changed_phone)
-        new_contact.append(contact[6])
-        new_contacts_list.append(new_contact)
-    return new_contacts_list
+pprint(final_contacts_list)
 
-
-def delete_duplicates_contact(new_contacts_list):
-    phone_book = dict()
-    for contact in new_contacts_list:
-        if contact[0] in phone_book:
-            contact_value = phone_book[contact[0]]
-            for i in range(len(contact_value)):
-                if contact[i]:
-                    contact_value[i] = contact[i]
-        else:
-            phone_book[contact[0]] = contact
-    return list(phone_book.values())
-
-
-def write_data(new_contacts_list):
-    with open("phonebook.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerows(new_contacts_list)
-
-
-new_contacts_list = input_data()
-
-new_parsed_list = parse_contact_list(new_contacts_list)
-
-contact_book_values = delete_duplicates_contact(new_parsed_list)
-
-write_data(contact_book_values)
+# TODO 2: сохраните получившиеся данные в другой файл
+with open("phonebook_new.csv", "w") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(final_contacts_list)
